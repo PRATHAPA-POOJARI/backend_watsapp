2924d1806304762f53f1039466f67b3d0232998c
prathappoojari607 <prathappoojari607@gmail.com>
2026-01-19 23:03:25 +0530

    addded  user model in  authController logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,39 +1,64 @@
 const nodemailer = require("nodemailer");
+const User = require("../models/user");
+const  sendMail = require("../utils/mailer");
 
-const sendOTP = async (req, res) => {
+
+exports.sendOTP = async (req, res) => {
   try {
-    const { email } = req.body;
-
-    if (!email) {
-      return res.status(400).json({ msg: "Email required" });
-    }
+    const { email, username } = req.body;
+    if (!email || !username)
+      return res.status(400).json({ msg: "Email & username required" });
 
     const otp = Math.floor(100000 + Math.random() * 900000);
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+    const user = await User.findOneAndUpdate(
+      { email },
+      {
+        username,
+        otp,
+        otpExpires: Date.now() + 5 * 60 * 1000,
       },
-    });
+      { upsert: true, new: true }
+    );
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Your OTP",
-      text: `Your OTP is ${otp}`,
-    });
+    await sendMail(email, otp);
 
-    res.status(200).json({
-      msg: "OTP sent successfully",
-      otp, // remove later (for testing only)
-    });
-
-  } catch (error) {
-    console.error("OTP Error:", error);
+    res.json({ msg: "OTP sent" });
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ msg: "OTP failed" });
   }
 };
 
-module.exports = { sendOTP };
+exports.verifyOTP = async (req, res) => {
+  const { email, otp } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ msg: "User not found" });
+
+  if (Date.now() > user.otpExpires)
+    return res.status(400).json({ msg: "OTP expired" });
+
+  if (user.otp !== otp)
+    return res.status(400).json({ msg: "Invalid OTP" });
+
+  user.otp = null;
+  user.otpExpires = null;
+  await user.save();
+
+  const token = jwt.sign(
+    { id: user._id, username: user.username, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.json({
+    msg: "Login success",
+    token,
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+};
